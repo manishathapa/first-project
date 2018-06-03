152c06bb211e56b0b03a7414e4cdc88b8a51e073
Manisha Thapa <manishathapa014@gmail.com>
2018-06-03 16:22:17 +0545

    remove space

--- src/Signup.js
+++ src/Signup.js
@@ -30,7 +30,7 @@ class Signup extends Component {
                 <input type="text" name="city" value={this.state.city} onChange={(event) => this.setState({city: event.target.value })} /> <br />
 
                 Password: <br />
-                <input type="text" name="password" value={this.state.password} onChange={(event) => this.setState({password: event.target.value })}/> <br />
+                <input type="text" name="password" value={this.state.password} onChange={(event) => this.setState({password: event.target.value })} /> <br />
                 <input type="submit" value="Submit" onClick={() => this.handleSubmit()} />
             </form>
         );
